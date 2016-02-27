726bfda3a03a75ae28bde3ce820d6f2b661dd1dc
ChairHacks <charisyoo@yahoo.com>
2016-02-27 16:41:05 -0500

    Update protosound.js

--- protosound.js
+++ protosound.js
@@ -9,3 +9,18 @@ var startIndex = inputURL.indexOf("#q=");
 searchInput = inputURL.substring(startIndex+4); //Create substring of search terms
 
 var searchArray = searchInput.split("+"); //Converts to array at '+' signs
+
+var end = 1;
+while(!condition)
+{
+if ((searchArray.length-end) != ("a", "it", "is", "the", "with", "an", "by", "to", "and"))
+{
+	condition = true;
+	searchtext = searchArray.length-end;
+	sendUrl = ("curl -X GET "https://api.spotify.com/v1/search?q=" + searchtext + "&type=track&market=US" -H "Accept: application/json"")
+}
+else
+{
+	condition = false;
+	end++:
+}}
