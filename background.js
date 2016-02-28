3f76dc24cbb5f7de0cf31ac2370fcd6323f4de41
ChairHacks <charisyoo@yahoo.com>
2016-02-28 10:05:54 -0500

    The Final Shit

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+schrome.browserAction.onClicked.addListener(function(tab) {
     var url = window.location.href.toString();
  	  if (tab.url.indexOf("http://www.google.com/")!= -1 || tab.url.indexOf("https://www.google.com/")!= -1) {
  	  console.log("ProtoSound");
@@ -7,21 +7,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
  	  }
     )}
 });
-
-/*
-chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-		function(tabs){
-				alert(tabs[0].url);
-		}
-);
-*/
-
-/*
-chrome.webRequest.onCompleted.addListener(
-	function(details) {
-		var inputURL = details.url;
-		chrome.runtime.sendMessage({greeting: inputURL}, function(response) {
-  		console.log("response.greeting")
-			});
-});
-*/
