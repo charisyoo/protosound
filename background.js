ec53914c8d58c1ac231f3f8a150dd90b9d51be97
ChairHacks <charisyoo@yahoo.com>
2016-02-28 05:36:36 -0500

    5:35

--- background.js
+++ background.js
@@ -1,8 +1,24 @@
-var inputURL;
+ chrome.browserAction.onClicked.addListener(function(tab){
+ 	if (tab.url.indexOf("http://www.google.com/")!= -1 || tab.url.indexOf("https://www.google.com/")!= -1) {
+ 	console.log("ProtoSound")
+ 	chrome.tabs.executeScript(tab.id, {
+ 		"file": "TEST.js"}
+ 	});
+ });
 
+/*
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+		function(tabs){
+				alert(tabs[0].url);
+		}
+);
+*/
+/*
 chrome.webRequest.onCompleted.addListener(
-	function(details) {           //Reads url on completion of event and
-		inputURL = details.url;     //assigns it to inputURL
-		chrome.details.sendmessage(detail.id, {inputURL}, function(response) {
-	});
+	function(details) {
+		var inputURL = details.url;
+		chrome.runtime.sendMessage({greeting: inputURL}, function(response) {
+  		console.log("response.greeting")
+			});
 });
+*?
